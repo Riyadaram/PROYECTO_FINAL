816243258056a46d66f6856193765423d95988bc
Vlad Beltran <puentes.studio@gmail.com>
2024-01-17 21:30:23 +0100

    some styles

--- src/App.jsx
+++ src/App.jsx
@@ -14,16 +14,10 @@ function App() {
           <img src={logotype} className="logo app" alt="Your Cloud logo" />
         </a>
       </div>
-      <div className="card">
-        {/* <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p> */}
-      </div>
+      
+      
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Upload and save your files, don't be shy!
       </p>
       <footer className='footer'>
         <Link to="/login" className="btn-primary login">Login</Link>
