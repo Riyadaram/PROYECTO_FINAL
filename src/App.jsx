d5c8e96881b665bfe7bd1172b7b971cb9cbfacfa
Vlad Beltran <puentes.studio@gmail.com>
2024-01-17 21:19:32 +0100

    splash and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 // import { useState } from 'react'
+import { Link, Route, Routes } from 'react-router-dom'
 import logotype from './assets/logo/logotype.png'
+// import Login from './components/Login'
 import './App.css'
 
 function App() {
@@ -12,17 +14,26 @@ function App() {
           <img src={logotype} className="logo app" alt="Your Cloud logo" />
         </a>
       </div>
-      {/* <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <div className="card">
+        {/* <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div> */}
+        </p> */}
+      </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <footer className='footer'>
+        <Link to="/login" className="btn-primary login">Login</Link>
+        <Link to="/register" className="btn-primary register">Register</Link>
+      </footer>
+
+      <Routes>
+        {/* <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Login />} /> */}
+      </Routes>
     </>
   )
 }
---
+++ src/components/Login.jsx
@@ -0,0 +1,35 @@
+// import { useState } from "react"
+// import { Link } from "react-router-dom"
+
+// const Login = () => {
+
+  
+//     return (
+//       <form onSubmit={handleForm}>
+//         <input
+//           name="username"
+//           placeholder="Usuario..."
+//           value={username}
+//           onChange={e => setUsername(e.target.value)}
+//         />
+//         <input
+//           name="password"
+//           placeholder="Contraseña..."
+//           type="password"
+//           value={password}
+//           onChange={e => setPassword(e.target.value)}
+//         />
+//         <button>Login</button>
+//         {error?.error &&
+//           <p className="error">Se ha producido un error: {error.error}</p>
+//         }
+//         <p>
+//           ¿No tienes cuenta? <Link to="/signup">Regístrate</Link>
+//         </p>
+//       </form>
+//     )
+//   }
+  
+//   export default Login
+  
+  
