2b7c9b9b8a590bab8ab98cd06851526e4fede567
Gonzalo_Rodríguez <gonzalo.jrodriguez@gmail.com>
2024-01-30 09:37:22 +0100

    ORDENAR PROYECTO, .ENV Y EXAMPLE.ENV

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom'
-import Header from '../Header/Header'
+import Header from '../../Header/Header'
 import './login.css'
-import FooterMenu from '../FooterMenu/FooterMenu'
+import FooterMenu from '../../FooterMenu/FooterMenu'
 
 
 const Login = () => {
