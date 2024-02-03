5caf4cd94ba842ad2cf51e54fd9fe21ee88cf416
Vlad Beltran <puentes.studio@gmail.com>
2024-02-03 12:43:00 +0000

    context header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,17 @@
 import { Link } from 'react-router-dom'
+import { AutenticacionContext } from '../../context/AutenticacionContext'
+import { useContext } from 'react'
 import logo from '../../assets/logo/logo.png'
 import './Header.css'
 
 const Header = () => {
 
-  
+    const { token } = useContext(AutenticacionContext);
+
     return (
         <nav className="nav-bar">
-                <Link to="/user-content">
-                 <img src={logo} className="logo app" alt="Your Cloud logo " />
+                <Link to={token ? '/user-content' : '/'}>
+                <img src={logo} className="logo app" alt="Your Cloud logo" />
                  </Link>
             </nav>
         )
